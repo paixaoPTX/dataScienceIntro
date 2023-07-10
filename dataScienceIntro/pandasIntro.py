d4b63aaf0920c3aa0713b2073d600d48f72ba3ad
Alorvaoa <aantunes@lnec.pt>
2023-07-10 18:08:48 +0100

    pandasPivot

--- dataScienceIntro/pandasIntro.py
+++ dataScienceIntro/pandasIntro.py
@@ -81,3 +81,26 @@ df.transpose()
 df = pd.DataFrame([[0, 1, -2, -1], [1, 1, 1, 1]])
 other = pd.DataFrame([[0, 1], [1, 2], [-1, -1], [2, 0]])
 df.dot(other)
+
+alunos = pd.DataFrame([["António", 1112], ["José", 1112], ["Sofia", 1112], ["Sofia", 1113], ["Sofia", 1114]], columns=["aluno", "codigoUC"])
+ucs = pd.DataFrame([[1111, "Java"], [1112, "Python"], [1113, "Python Avançado"]], columns=["codigoUC", "nomeUC"])
+pd.merge(alunos, ucs, on  = ["codigoUC"], how="inner")
+
+#Exemplo Pivot Tables
+import numpy as np
+
+data = {'Category': ['A', 'A', 'B', 'B', 'A', 'B'], 
+        'Year': ['2019', '2019', '2020', '2020', '2021', '2021'], 
+        'Quantity': [10, 15, 20, 5, 12, 8], 
+        'Price': [100, 150, 200, 50, 120, 80]}
+
+df = pd.DataFrame(data)
+pivot_sum = pd.pivot_table(df, values='Quantity', index='Category', columns='Year', aggfunc=np.sum)
+print(pivot_sum)
+pivot_count = pd.pivot_table(df, values='Quantity', index='Category', columns='Year', aggfunc='count')
+print(pivot_count)
+pivot_mean = pd.pivot_table(df, values='Quantity', index='Category', columns='Year', aggfunc=np.mean)
+print(pivot_mean)
+pivot = pd.pivot_table(df, values='Quantity', index='Category', columns='Year',
+                             aggfunc=['count', np.sum])
+print(pivot)
